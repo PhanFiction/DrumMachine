f4e22524313e8a1896d198cd8c4c635236577036
Steve <stevewu24@gmail.com>
2023-11-18 22:24:29 -0800

    Added color palette to styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blackBG: '#343434',
-        cardBlack: '#505050',
+        turqoise: '#CFDBD5',
+        lightTurqoise: '#E8EDDF',
+        yellow: '#F5CB5C',
+        dark: '#242423',
+        gray: '#D9D9D9',
       }
     },
   },
